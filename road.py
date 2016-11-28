2862bf3257e7142755aaedbc72905d62519ee4d6
Alexandra Zinina <zinina.a@gmail.com>
2016-11-28 17:27:04 +0300

    поправки, дубль два, проблемы с 53-2 сохраняются, тесты для 51 формирует6

--- road.py
+++ road.py
@@ -2,50 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-# import cx_Oracle
 import datetime
 import os
 import sys
 import traceback
 
-#ADD_QUERY_TEMPLATE = """
-#BEGIN
-#   onm_api_crms.api_emul.create_passage_trx(
-#     p_plaza_out => '643:00042:01:{}', 
-#     p_plaza_in => '643:00042:01:{}',
-#     p_pan => '{}',
-#     p_class => '{}',
-#     p_cnt => {},
-#     p_dt => to_date('{}', 'yyyy-mm-dd hh24:mi:ss'));
-#END;
-#"""
-#LOGIN = "o"
-#PSW = "o"
-#DNS = "10"
+PATH = '643:1:1:'
 
-road51 = [[51,0,True,False],
-          [53,2500,True,False],
-          [2,2500,False,True],
-          [1,2550,True,False],
-          [4,4750,False,True],
-          [5,4750,False,False],
-          [8,4750,True,False],
-          [55,8650,False,True],
-          [9,8650,True,False],
-          [61,17450,False,True],
-          [63,17450,True,False],
-          [65,19650,True,False],
-          [11,22550,False,True],
-          [67,22550,True,False],
-          [17,31050,False,True],
-          [77,39550,True,False],
-          [78,39550,True,False],
-          [19,53750,False,True]]
+road51 = [[51,0,True,False,False],
+          [53,2500,True,False,False],
+          [2,2530,False,True,False],
+          [1,2551,True,False,False],
+          [4,4750,False,True,False],
+          [5,4751,False,False,True],
+          [8,4752,True,False,False],
+          [55,8650,False,True,False],
+          [9,8651,True,False,False],
+          [61,17450,False,True,False],
+          [63,17451,True,False,False],
+          [65,19650,True,False,False],
+          [11,22550,False,True,False],
+          [67,22551,True,False,False],
+          [17,31050,False,True,True],
+          [77,39551,True,False,False],
+          [78,39552,True,False,False],
+          [19,53750,False,True,False]]
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='tag date road')
     parser.add_argument("--tag", type=str, default = '01')
-    parser.add_argument("--date", type=str, default = datetime.datetime.now().strftime('%Y-%m-%d %X'))
+    parser.add_argument("--date", type=str, default = datetime.datetime.now().strftime('%d.%m.%Y %X'))
     parser.add_argument("--road", type=int, required = True, choices = [51,52])
     return parser.parse_args()
 
@@ -58,59 +44,55 @@ def check_arguments(settings):
         settings.date = datetime.datetime.now()
     else:    
         try:
-            settings.date = datetime.datetime.strptime(settings.date, '%Y-%m-%d %X')
+            settings.date = datetime.datetime.strptime(settings.date, '%d.%m.%Y %X')
         except:
             raise Exception("Неверный формат даты")
         if abs(settings.date.year - datetime.datetime.now().year) > 1:
             raise Exception('Слишком отдаленный период времени')
     
 
-def pp(settings):
-    date = settings.date.strftime('%Y-%m-%d %X')
+def make_routs(settings):
+    allrouts = []
     if settings.road == 51:
-        r = road51
+        road = road51
     else:
-        r = road52
-    for i in r:
+        road = road52
+    for i in road:
         if not i[2]:
             continue
         route = [i]
-        for l in r:
+        for l in road:
             if l[1] < i[1] or l[2]:
                 continue
-            if not l[3]:
+            if l[4]:
                 route.append(l)
             if l[3]:
-                print(route + [l])
+                #print(route + ([l] if not l[4] else []))
+                allrouts.append(route + ([l] if not l[4] else []))
+    return allrouts            
     
+    
+    
+def make_csv(allrouts,settings):
+    file = open('/home/zinina/routs/test.csv', 'w') #открываю файл на запись
+    
+    for i in allrouts:
+        for l in i:
+            print(l)
+            plaza = PATH + str(l[0]) #формирую точку вьезда вида 643:1:1:узел
+            p_time = round((l[1]/50000)*60) #рассчет времени до этой точки в минутах
+            pt = 0
+            pt = pt + p_time
+            p_date = settings.date + datetime.timedelta(minutes=p_time) #прибавление полученного интервала к указанной дате
+            file.write(settings.tag + ';' + p_date.strftime('%d.%m.%Y %X') + ';' + plaza + ';'  + '\n') #формирование строки csv файла
+            #p_date = p_date + datetime.timedelta(minutes=15) #двигаю исходную дату для чистоты эксперимента
+        settings.date = settings.date + datetime.timedelta(minutes=pt) + datetime.timedelta(minutes=1)
+        
+    file.close()            
+                             
 
-# def pp2(settings):
-#     crms_connection = None
-#     crms_cursor = None
-#     date = settings.date.strftime('%Y-%m-%d %X')
-#     query = ADD_QUERY_TEMPLATE.format(settings.inn, settings.out, settings.pan, settings.ts, settings.count, date)
-    # print(query)
-    #exit_code = os.system('echo "{}" | ./dummy_sqlplus.sh'.format(query))
-    #print("exit code: {}".format(exit_code))
-#     query_api_login = "BEGIN :k := onm.ac.login('l', 's', 'C'); onm.ac.init(:k); onm.ac.set_position(31); END;"
-#     try:
-#         crms_connection = cx_Oracle.connect(LOGIN, PSW, DNS)
-#         crms_cursor = crms_connection.cursor()    
-#         key = crms_cursor.var(cx_Oracle.STRING)
-#         crms_cursor.execute(query_api_login, k = key)
-#         crms_cursor.execute(query)
-#         print(query)
-#         crms_connection.commit()
-#         print("commited")
-#     except cx_Oracle.DatabaseError as e:
-#         error, = e.args
-#         print(error.message)
-#     if crms_cursor:
-#         crms_cursor.close()
-#     if crms_connection:
-# crms_connection.close()
 
-def p():
+def prime():
     try:
         settings = parse_arguments()
         check_arguments(settings)
@@ -118,10 +100,11 @@ def p():
         print('Неверные параметры: ' + str(e))
         return
     print(settings)
-    pp(settings)    
+    print(make_routs(settings))
+    make_csv(make_routs(settings),settings)    
 
 def main():
-    p()  
+    prime()  
     
 if __name__ == '__main__':
     main()
